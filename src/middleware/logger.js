12e7443f52d8ac2fd74395c3da45d9477e5ca1a9
Sun-007 <138206213+Umesh-WW@users.noreply.github.com>
2024-01-02 11:24:34 +0530

    created routes for auth

---
+++ src/middleware/logger.js
@@ -0,0 +1,27 @@
+let count = 0;
+const cookieLogger = (req, _res, next) => {
+  console.log("log==------------------>>", req.url, req.body);
+  console.log("\n==============================");
+  console.log(`------------>  ${count++}`);
+
+  console.log(`req.body.username -------> ${req.body.username}`);
+  console.log(`req.body.password -------> ${req.body.password}`);
+
+  console.log(`\n req.session.passport -------> `);
+  console.log(req.session.passport);
+
+  console.log(`\n req.user -------> `);
+  console.log(req.user);
+
+  console.log("\n Session and Cookie");
+  console.log(`req.session.id -------> ${req.session.id}`);
+  console.log(`req.session.cookie -------> `);
+  console.log(req.session.cookie);
+
+  console.log("===========================================\n");
+  next();
+};
+
+const portLogger = () =>
+  console.log(`server is running on port ${process.env.PORT || 3000}`);
+module.exports = { cookieLogger,  };
---
+++ src/routes/auth.js
@@ -0,0 +1,75 @@
+// auth.js
+
+const express = require("express");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const router = express.Router();
+
+const USER = [
+  { id: 1, username: "admin", password: "password" },
+  { id: 2, username: "test", password: "test" },
+];
+
+const authUser = (user, password, done) => {
+  const authenticated_user = USER.find(
+    (users) => users.username === user && users.password === password
+  );
+
+  if (!authenticated_user) {
+    return done(null, false, { message: "user not found!" });
+  }
+
+  return done(null, authenticated_user);
+};
+
+passport.use(new LocalStrategy(authUser));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Authentication failed", info: info });
+    }
+
+    req.login(user, (loginErr) => {
+      if (loginErr) {
+        return next(loginErr);
+      }
+
+      return res.json({ message: "Authentication successful", user });
+    });
+  })(req, res, next);
+});
+
+router.use((req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(202).json({ message: "not authed!" });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).json({ message: "Logout failed", error: err });
+    }
+    res.json({ message: "log out!" });
+  });
+});
+
+module.exports = router;
