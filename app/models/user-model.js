31f6de5e79b09774a9091a605d018bf30588e880
Sun-007 <78736365+Umesh-daiict@users.noreply.github.com>
2024-01-05 22:32:36 +0530

    create pre and bycrpt function

--- app/models/user-model.js
+++ app/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const users = new mongoose.Schema({
 	username: {
 		type: String,
@@ -10,4 +11,28 @@ const users = new mongoose.Schema({
 		required: true,
 	},
 });
+// store multi img and compress it
+users.pre('save', async function (next) {
+	const user = this;
+	if (!user.isModified('password')) return next();
+
+	try {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(user.password, salt);
+
+		user.password = hashedPassword;
+		next();
+	} catch (err) {
+		return next(err);
+	}
+});
+
+users.methods.comparePassword = async function (enteredPassword) {
+	try {
+		return await bcrypt.compare(enteredPassword, this.password);
+	} catch (err) {
+		throw err;
+	}
+};
+
 module.exports = mongoose.model('users', users);
